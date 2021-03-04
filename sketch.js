5e23d7a4b07e5d398d28790f8eb34723fb625ea1
Matt Payne <Payne@MattPayne.org>
2021-03-04 17:01:32 -0600

    Screens are now switching.

--- sketch.js
+++ sketch.js
@@ -1,13 +1,43 @@
-let n = 1;
+let [n, screenNum] = [1,0];
 let [start, increment] = [30, 30];
 const byg = [ 'blue', 'yellow', 'green' ];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let button = createButton('Switch');
+  button.position(0,0);
+  button.mousePressed(switchScreen);
   noLoop();
 }
 
+function switchScreen() {
+  screenNum++;
+  screenNum %= 2;
+  console.log(screenNum + ' = screenNum now.');
+  draw(); // since noLoop() is on...
+}
+
 function draw() {
+  switch (screenNum) {
+    case 0:
+      draw0();
+      break;
+    case 1:
+      draw1();
+      break;
+  }
+}
+
+function draw1() {
+  background(220);
+  fill('black');
+  textSize(24);
+  text('this is draw1 method.', 10, 50);
+}
+
+function draw0() {
+  background(220);
+  fill('black');
   for (let y = start; y < height; y += increment) {
     for (let x = start; x < width; x += increment) {
       fillSquare(x, y); 
